649293c043171662b925c047000ae37afab3d29f
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2022-08-30 01:32:32 +0600

    dashoard

--- src/componentes/Header/Header.js
+++ src/componentes/Header/Header.js
@@ -1,17 +1,16 @@
-import { signOut } from 'firebase/auth';
-import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { signOut } from "firebase/auth";
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 // import {Link} from "react-router-dom"
-import { Link, useNavigate } from 'react-router-dom';
-import auth from '../../firebase.int';
-
+import { Link, useNavigate } from "react-router-dom";
+import auth from "../../firebase.int";
 
 const Header = () => {
   const [user, loading, error] = useAuthState(auth);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const logout = () => {
-    signOut (auth);
-    navigate("/login")
+    signOut(auth);
+    navigate("/login");
   };
 
   const liItems = (
@@ -19,7 +18,6 @@ const Header = () => {
       <li>
         <Link to="/">Home</Link>
       </li>
-      
 
       <li>
         <Link to="/cv-writing">CV WRITING</Link>
@@ -30,47 +28,77 @@ const Header = () => {
       <li>
         <Link to="/profile">Examples</Link>
       </li>
-    
-      
-   
-      
-      
-      
     </>
   );
 
-     return (
-        
-               <div class="navbar bg-base-100">
-  <div class="navbar-start">
-    <div class="dropdown">
-      <label tabindex="0" class="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-      </label>
-      <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-       {liItems}
-      </ul>
+  return (
+    <div class="navbar bg-base-100">
+      <div class="navbar-start">
+        <div class="dropdown">
+          <label tabindex="0" class="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabindex="0"
+            class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {liItems}
+          </ul>
+        </div>
+        <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+      </div>
+      <div class="navbar-center hidden lg:flex">
+        <ul class="menu menu-horizontal p-0">{liItems}</ul>
+      </div>
+      <div class="navbar-end">
+        {/* <a >Get started</a> */}
+        <span className=" font-1xl">{user?.email}</span>
+        {user ? (
+          <button className="ml-2 btn btn-outline" onClick={logout}>
+            Signout
+          </button>
+        ) : (
+          <Link className="btn" to="/login">
+            Login
+          </Link>
+        )}
+      </div>
+
+      <div class="dropdown dropdown-end">
+        <label tabindex="0" class=" ">
+          <div class="avatar">
+            <div class="w-12 rounded-full  ">
+              <img src="https://placeimg.com/192/192/people" />
+            </div>
+          </div>
+        </label>
+        <ul
+          tabindex="0"
+          class="menu dropdown-content p-2 shadow bg-base-100 rounded-box w-52 mt-4"
+        >
+          <li>
+            <a>Item 1</a>
+          </li>
+          <li>
+            <a>Item 2</a>
+          </li>
+        </ul>
+      </div>
     </div>
-    <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
-  </div>
-  <div class="navbar-center hidden lg:flex">
-    <ul class="menu menu-horizontal p-0">
-     {liItems}
-    </ul>
-  </div>
-  <div class="navbar-end">
-    {/* <a >Get started</a> */}
-    <span className=' font-1xl'>{user?.email}</span>
-  {user?  <button className='ml-2 btn btn-outline' onClick={logout}>Signout</button>: <Link className="btn" to = "/login">
-      Login
-    </Link>}
-   
-   
-   
-  </div>
-</div>
-          
-     );
+  );
 };
 
 export default Header;
